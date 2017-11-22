096ae3c15d0d0e84c682ce2e7e424527d5883d41
jason <jkinney23@yahoo.ca>
2017-11-23 01:44:48 +0400

    Interrupts complete

--- kernel.cpp
+++ kernel.cpp
@@ -1,5 +1,6 @@
 #include "types.h"
 #include "gdt.h"
+#include "interrupts.h"
 
 void printf(char* str)
 {
@@ -55,6 +56,11 @@ extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot
     printf("Hello Worlds Again!\n");
 
 	GlobalDescriptorTable gdt;
+	InterruptManager interrupts(&gdt);
+	
+	
+	
+	interrupts.Activate();
 
     while(1);
 }
