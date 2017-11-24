31e90572f39efb0119dd78942ba0af456ebbfa0b
jason <jkinney23@yahoo.ca>
2017-11-24 09:54:42 +0400

    Keyboard driver works!

--- keyboard.h
+++ keyboard.h
@@ -1,26 +1,10 @@
 #ifndef __KEYBOARD_H
 #define __KEYBOARD_H
 
-<<<<<<< HEAD
-=======
-
->>>>>>> refs/remotes/origin/master
 #include "types.h"
 #include "interrupts.h"
 #include "port.h"
 
-<<<<<<< HEAD
-class KeyboardDriver : public InterruptHandler
-{
-	Port8Bit dataport;
-	Port8Bit commandport;
-public:
-	KeyboardDriver(InterruptManager* manager);
-	~KeyboardDriver();
-	virtual uint32_t HandleInterrupt(uint32_t esp);
-};
-
-=======
 
 class KeyboardDriver : public InterruptHandler
 {
@@ -33,5 +17,4 @@ public:
 };
 
 
->>>>>>> refs/remotes/origin/master
 #endif
